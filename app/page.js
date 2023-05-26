62713a1c6c85c2b561603581f7a715ef42c162bb
DocSJ1980 <docsaj@outlook.com>
2023-05-26 13:18:08 +0500

    Register form added, next-auth basic setup done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
-import Image from 'next/image'
-
-export default function Home() {
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from './api/auth/[...nextauth]/route'
+import User from './components/user'
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  console.log(session)
   return (
     <section>
-      Home
+      <h2>Home</h2>
+      <h2>Server Side Rendered </h2>
+      <pre>
+        {JSON.stringify(session)}
+      </pre>
+      <h2>Client Side Rendered </h2>
+      <User />
     </section>
   )
 }
