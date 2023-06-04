f77a68bdf651c3295f50567117721555b9dc520e
DocSJ1980 <docsaj@outlook.com>
2023-06-04 23:18:06 +0500

    Save current progress

--- app/libs/jwt.js
+++ app/libs/jwt.js
@@ -3,16 +3,15 @@ import jwt from "jsonwebtoken";
 const DEFAULT_SIGN_OPTION = {
     expiresIn: "1h",
 };
+const secret_key = process.env.SECRET;
 
 export function signJwtAccessToken(payload, options = DEFAULT_SIGN_OPTION) {
-    const secret_key = process.env.SECRET;
     const token = jwt.sign(payload, secret_key, options);
     return token;
 }
 
 export function verifyJwt(token) {
     try {
-        const secret_key = process.env.SECRET_KEY;
         const decoded = jwt.verify(token, secret_key);
         return decoded;
     } catch (error) {
